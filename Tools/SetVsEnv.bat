4f29ed1b67b42e21961e1e9c9c1dce0dc3d4208b
Yuri Benditovich <yuri.benditovich@daynix.com>
2021-04-20 09:45:53 +0300

    build: use EWDK 2004
    Signed-off-by: Yuri Benditovich <yuri.benditovich@daynix.com>

--- Tools/SetVsEnv.bat
+++ Tools/SetVsEnv.bat
@@ -1,8 +1,10 @@
 @echo off
 if not "%EnterpriseWDK%"=="" goto ready
-if "%EWDK1903_DIR%"=="" set EWDK1903_DIR=c:\ewdk1903
-call :add_path "%EWDK1903_DIR%\Program Files\Microsoft Visual Studio\2019\BuildTools\VC\Redist\MSVC\14.20.27508\onecore\x86\Microsoft.VC141.OPENMP\vcomp140.dll"
-call %EWDK1903_DIR%\BuildEnv\SetupBuildEnv.cmd
+if "%EWDK2004_DIR%"=="" set EWDK2004_DIR=c:\ewdk2004
+call :add_path "%EWDK2004%\Program Files\Microsoft Visual Studio\2019\BuildTools\VC\Redist\MSVC\14.27.29016\onecore\x86\Microsoft.VC142.OPENMP\vcomp140.dll"
+call :add_path "%EWDK2004%\Program Files\Windows Kits\10\bin\10.0.19041.0\x86\ucrt\ucrtbased.dll"
+call :add_path "%EWDK2004%\Program Files\Microsoft Visual Studio\2019\BuildTools\VC\Redist\MSVC\14.27.29016\onecore\debug_nonredist\x86\Microsoft.VC142.DebugCRT\vcruntime140D.dll"
+call %EWDK2004%\BuildEnv\SetupBuildEnv.cmd
 goto :eof
 
 :add_path
